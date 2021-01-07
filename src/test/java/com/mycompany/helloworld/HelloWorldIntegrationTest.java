9075b14aa4ff563d671f42774986e699cb48016f
Shiva Saxena <shivasaxena911@gmail.com>
2021-01-07 16:53:51 +0530

    fix(test): add data assertions

--- src/test/java/com/mycompany/helloworld/HelloWorldIntegrationTest.java
+++ src/test/java/com/mycompany/helloworld/HelloWorldIntegrationTest.java
@@ -1,12 +1,11 @@
 package com.mycompany.helloworld;
 
-//import com.mycompany.helloworld.HelloWorldConfiguration;
-//import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.*;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import io.dropwizard.testing.ResourceHelpers;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.client.Client;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,11 +16,15 @@ public class HelloWorldIntegrationTest {
             new DropwizardAppRule<HelloWorldConfiguration>(HelloWorldApplication.class, ResourceHelpers.resourceFilePath("config.yml"));
 
     @Test
-    public void loginHandlerRedirectsAfterPost() {
+    public void TestGetRequestOnHomeURL() {
         Client client = RULE.client();
         Response response = client.target(
                 String.format("http://localhost:%d", RULE.getLocalPort()))
                 .request().get();
         assertThat(response.getStatus()).isEqualTo(200);
+        Map data = response.readEntity(Map.class);
+        assertThat(data.get("id")).isEqualTo(1);
+        assertThat((data.get("content"))).isEqualTo("Hello World!");
     }
+
 }
